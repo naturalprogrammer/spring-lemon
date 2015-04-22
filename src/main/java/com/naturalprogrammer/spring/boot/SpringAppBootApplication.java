d6fecbd23c49b7af459fbde67d40e09a5a2760cc
Sanjay <skpatel20@gmail.com>
2015-04-22 10:42:27 +0530

    Just after generating the project using Spring Starter Project Wizard

---
+++ src/main/java/com/naturalprogrammer/spring/boot/SpringAppBootApplication.java
@@ -0,0 +1,12 @@
+package com.naturalprogrammer.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppBootApplication.class, args);
+    }
+}
