7df3b07c50fa9467fdd1aa27acd67eb39d33c23a
Sanjay <skpatel20@gmail.com>
2015-07-31 09:25:00 +0530

    Simplified JsonPrefix for JSON vulnerability

--- src/main/java/com/naturalprogrammer/spring/lemon/JsonPrefixWebConfig.java
+++ src/main/java/com/naturalprogrammer/spring/lemon/JsonPrefixWebConfig.java
@@ -1,43 +1,43 @@
-package com.naturalprogrammer.spring.lemon;
-
-import java.util.List;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-/**
- * If you want to disable this, e.g. while testing or in pure REST APIs,
- * in your application.properties, use
- * 
- * lemon.jsonprefix.enabled: false
- *
- * https://docs.angularjs.org/api/ng/service/$http
- * http://stackoverflow.com/questions/26384930/how-to-add-n-before-each-spring-json-response-to-prevent-common-vulnerab
- * http://docs.spring.io/spring-data/jpa/docs/current/reference/html/#core.web
- * @author Sanjay Patel
- *
- */
-@Configuration
-@ConditionalOnProperty(name="lemon.enabled.jsonprefix", matchIfMissing=true)
-public class JsonPrefixWebConfig extends WebMvcConfigurerAdapter {
-	
-	private final Log log = LogFactory.getLog(getClass());
-	
-	public final static String JSON_PREFIX = ")]}',\n";
-	
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-    	
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setJsonPrefix(JSON_PREFIX);
-        converters.add(converter);
-        
-        log.info("Configured prefixing JSON with )]}... for JSON vulnerability");
-    }
-    
-}
+//package com.naturalprogrammer.spring.lemon;
+//
+//import java.util.List;
+//
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+///**
+// * If you want to disable this, e.g. while testing or in pure REST APIs,
+// * in your application.properties, use
+// * 
+// * lemon.jsonprefix.enabled: false
+// *
+// * https://docs.angularjs.org/api/ng/service/$http
+// * http://stackoverflow.com/questions/26384930/how-to-add-n-before-each-spring-json-response-to-prevent-common-vulnerab
+// * http://docs.spring.io/spring-data/jpa/docs/current/reference/html/#core.web
+// * @author Sanjay Patel
+// *
+// */
+//@Configuration
+//@ConditionalOnProperty(name="lemon.enabled.jsonprefix", matchIfMissing=true)
+//public class JsonPrefixWebConfig extends WebMvcConfigurerAdapter {
+//	
+//	private final Log log = LogFactory.getLog(getClass());
+//	
+//	public final static String JSON_PREFIX = ")]}',\n";
+//	
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//    	
+//        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+//        converter.setJsonPrefix(JSON_PREFIX);
+//        converters.add(converter);
+//        
+//        log.info("Configured prefixing JSON with )]}... for JSON vulnerability");
+//    }
+//    
+//}
--- src/main/java/com/naturalprogrammer/spring/lemon/LemonConfig.java
+++ src/main/java/com/naturalprogrammer/spring/lemon/LemonConfig.java
@@ -2,10 +2,12 @@ package com.naturalprogrammer.spring.lemon;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
@@ -17,8 +19,32 @@ import org.springframework.web.client.RestTemplate;
 @EnableAsync
 public class LemonConfig {
 	
+	public final static String JSON_PREFIX = ")]}',\n";
+
 	private final Log log = LogFactory.getLog(getClass());
 
+	/**
+	 * Prefixes JSON responses for JSON vulnerability.
+	 * 
+	 * To disable this, in your application.properties, use
+	 * 
+	 * lemon.jsonprefix.enabled: false
+	 *
+	 * https://docs.angularjs.org/api/ng/service/$http
+	 * http://stackoverflow.com/questions/26384930/how-to-add-n-before-each-spring-json-response-to-prevent-common-vulnerab
+	 */
+	@Bean
+	@ConditionalOnProperty(name="lemon.enabled.jsonprefix", matchIfMissing=true)
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+		
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setJsonPrefix(JSON_PREFIX);
+        
+        log.info("Configuring JSON vulnerability prefix ...");
+        
+        return converter;
+	}
+	
 	@Bean
 	public RestTemplate restTemplate() {
 		
