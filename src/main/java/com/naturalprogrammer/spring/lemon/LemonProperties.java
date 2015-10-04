319280862271cf0a703d1012dbc3eaabbde664f9
Sanjay <skpatel20@gmail.com>
2015-10-04 16:48:33 +0530

    Trimmed Access-Control-Allow-Headers. Kept only the custom ones. I.e., removed "origin", "content-type", "accept"

--- src/main/java/com/naturalprogrammer/spring/lemon/LemonProperties.java
+++ src/main/java/com/naturalprogrammer/spring/lemon/LemonProperties.java
@@ -159,12 +159,12 @@ public class LemonProperties {
 		private String[] allowedMethods = {"GET", "HEAD", "POST", "PUT", "DELETE", "TRACE", "OPTIONS", "PATCH"};
 		
 		/**
-		 * Headers to be allowed, e.g. origin,content-type,...
+		 * Custom headers to be allowed, e.g. x-requested-with,x-xsrf-token
 		 */
-		private String[] allowedHeaders = {"x-requested-with", "origin", "content-type", "accept", LemonCsrfFilter.XSRF_TOKEN_HEADER_NAME};
+		private String[] allowedHeaders = {"x-requested-with", LemonCsrfFilter.XSRF_TOKEN_HEADER_NAME};
 		
 		/**
-		 * Response headers you want to expose to the client JavaScript programmer, e.g. "X-XSRF-TOKEN". <br>
+		 * Custom response headers you want to expose to the client JavaScript programmer, e.g. "X-XSRF-TOKEN". <br>
 		 * See <a href="http://stackoverflow.com/questions/25673089/why-is-access-control-expose-headers-needed#answer-25673446">
 		 * here</a> to know why this could be needed.
 		 */		
