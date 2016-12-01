38d245c8ae24d8c6ffd75719a6903e7e07b15d91
Sanjay <skpatel20@gmail.com>
2016-12-01 10:11:28 +0530

    0.10: Onto migrating to gradle

---
+++ spring-lemon/src/main/java/com/naturalprogrammer/spring/lemon/LemonController.java
@@ -0,0 +1,200 @@
+package com.naturalprogrammer.spring.lemon;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.naturalprogrammer.spring.lemon.domain.AbstractUser;
+import com.naturalprogrammer.spring.lemon.domain.ChangePasswordForm;
+import com.naturalprogrammer.spring.lemon.util.LemonUtil;
+
+/**
+ * The Lemon API
+ * 
+ * @author Sanjay Patel
+ *
+ * @param <U>	The User class
+ * @param <ID>	The Primary key type of User class 
+ */
+public abstract class LemonController
+	<U extends AbstractUser<U,ID>, ID extends Serializable> {
+
+	private static final Log log = LogFactory.getLog(LemonController.class);
+
+	private LemonService<U, ID> lemonService;
+	
+	@Autowired
+	public void setLemonService(LemonService<U, ID> lemonService) {
+		this.lemonService = lemonService;
+	}
+
+
+	/**
+	 * A simple function for pinging this server.
+	 */
+	@GetMapping("/ping")
+	public void ping() {
+		log.debug("Received a ping");
+	}
+	
+
+	/**
+	 * Returns context properties needed at the client side, and
+	 * the current-user data.
+	 */
+	@GetMapping("/context")
+	public Map<String, Object> getContext() {
+		
+		Map<String, Object> context =
+			LemonUtil.mapOf("context", lemonService.getContext(),
+							"user", lemonService.userForClient());
+		
+		log.debug("Returning context: " + context);
+
+		return context;
+	}
+	
+
+	/**
+	 * Signs up a user, and logs him in.
+     *
+	 * @param user	data fed by the user
+	 * @return data about the logged in user
+	 */
+	@PostMapping("/signup")
+	@ResponseStatus(HttpStatus.CREATED)
+	public U signup(@RequestBody U user) {
+		
+		log.debug("Signing up: " + user);
+		lemonService.signup(user);
+		log.debug("Signed up: " + user);
+		
+		return lemonService.userForClient();
+	}
+	
+	
+	/**
+	 * Resends verification mail. 
+	 */
+	@GetMapping("/users/{id}/resend-verification-mail")
+	public void resendVerificationMail(@PathVariable("id") U user) {
+		
+		log.debug("Resending verification mail for: " + user);
+		lemonService.resendVerificationMail(user);
+		log.debug("Resent verification mail for: " + user);
+	}	
+
+
+	/**
+	 * Verifies current-user.
+	 */
+	@PostMapping("/users/{verificationCode}/verify")
+	public U verifyUser(@PathVariable String verificationCode) {
+		
+		log.debug("Verifying user ...");		
+		lemonService.verifyUser(verificationCode);
+		
+		return lemonService.userForClient();
+	}
+	
+
+	/**
+	 * The forgot Password feature.
+	 */
+	@PostMapping("/forgot-password")
+	public void forgotPassword(@RequestParam String email) {
+		
+		log.debug("Received forgot password request for: " + email);				
+		lemonService.forgotPassword(email);
+	}
+	
+
+	/**
+	 * Resets password after it is forgotten.
+	 */
+	@PostMapping("/users/{forgotPasswordCode}/reset-password")
+	public void resetPassword(@PathVariable String forgotPasswordCode,
+							  @RequestParam String newPassword) {
+		
+		log.debug("Resetting password ... ");				
+		lemonService.resetPassword(forgotPasswordCode, newPassword);
+	}
+
+
+	/**
+	 * Fetches a user by email.
+	 */
+	@GetMapping("/users/fetch-by-email")
+	public U fetchUserByEmail(@RequestParam String email) {
+		
+		log.debug("Fetching user by email: " + email);						
+		return lemonService.fetchUserByEmail(email);
+	}
+
+	
+	/**
+	 * Fetches a user by Id.
+	 */	
+	@GetMapping("/users/{id}/fetch-by-id")
+	public U fetchUserById(@PathVariable("id") U user) {
+		
+		log.debug("Fetching user: " + user);				
+		return lemonService.processUser(user);
+	}
+
+	
+	/**
+	 * Updates a user.
+	 */
+	@PostMapping("/users/{id}/update")
+	public U updateUser(@PathVariable("id") U user, @RequestBody U updatedUser) {
+		
+		log.debug("Updating user ... ");				
+		lemonService.updateUser(user, updatedUser);
+		return lemonService.userForClient();		
+	}
+	
+	
+	/**
+	 * Changes password.
+	 */
+	@PostMapping("/users/{id}/change-password")
+	public void changePassword(@PathVariable("id") U user,
+			@RequestBody ChangePasswordForm changePasswordForm) {
+		
+		log.debug("Changing password ... ");				
+		lemonService.changePassword(user, changePasswordForm);
+	}
+
+
+	/**
+	 * Requests for changing email.
+	 */
+	@PostMapping("/users/{id}/request-email-change")
+	public void requestEmailChange(@PathVariable("id") U user,
+								   @RequestBody U updatedUser) {
+		
+		log.debug("Requesting email change ... ");				
+		lemonService.requestEmailChange(user, updatedUser);
+	}
+	
+	/**
+	 * Changes the email.
+	 */
+	@PostMapping("/users/{changeEmailCode}/change-email")
+	public void changeEmail(@PathVariable String changeEmailCode) {
+		
+		log.debug("Changing email of user ...");		
+		lemonService.changeEmail(changeEmailCode);
+	}
+}
---
+++ spring-lemon/src/main/java/com/naturalprogrammer/spring/lemon/security/LemonSecurityConfig.java
@@ -0,0 +1,287 @@
+package com.naturalprogrammer.spring.lemon.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.rememberme.AbstractRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.switchuser.SwitchUserFilter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+import com.naturalprogrammer.spring.lemon.LemonProperties;
+
+
+/**
+ * Security configuration class. Extend it in the
+ * application, and make a configuration class. Override
+ * protected methods, if you need any customization.
+ * 
+ * @author Sanjay Patel
+ */
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public abstract class LemonSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	// Computed authorities
+	public static final String GOOD_ADMIN = "GOOD_ADMIN";
+	public static final String GOOD_USER = "GOOD_USER";
+	
+	// remember-me related
+	public static final String REMEMBER_ME_COOKIE = "rememberMe";
+	public static final String REMEMBER_ME_PARAMETER = "rememberMe";
+	
+	protected LemonProperties properties;
+	protected UserDetailsService userDetailsService;
+	protected AuthenticationSuccessHandler authenticationSuccessHandler;
+	protected LogoutSuccessHandler logoutSuccessHandler;
+	
+	@Autowired
+	public void setProperties(LemonProperties properties) {
+		this.properties = properties;
+	}
+
+	@Autowired
+	public void setUserDetailsService(UserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+
+	@Autowired
+	public void setAuthenticationSuccessHandler(AuthenticationSuccessHandler authenticationSuccessHandler) {
+		this.authenticationSuccessHandler = authenticationSuccessHandler;
+	}
+
+	@Autowired
+	public void setLogoutSuccessHandler(LogoutSuccessHandler logoutSuccessHandler) {
+		this.logoutSuccessHandler = logoutSuccessHandler;
+	}
+
+	/**
+	 * Authentication failure handler, to override the default behavior
+	 * of spring security -  redirecting to the login screen 
+	 */
+	@Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+    	return new SimpleUrlAuthenticationFailureHandler();
+    }	
+	
+	/**
+	 * Password encoder
+	 */
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+    }
+	
+
+	/**
+	 * Security configuration, calling protected methods
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		login(http); // authentication
+		logout(http); // logout related configuration
+		exceptionHandling(http); // exception handling
+		rememberMe(http); // remember-me
+		csrf(http); // csrf configuration
+		switchUser(http); // switch-user configuration
+		authorizeRequests(http); // authorize requests
+		otherConfigurations(http); // override this to add more configurations
+	}
+
+
+	/**
+	 * Configuring authentication.
+	 * 
+	 * @param http
+	 * @throws Exception
+	 */
+	protected void login(HttpSecurity http) throws Exception {
+		
+		http
+		.formLogin() // cookie based form login
+			
+			/******************************************
+			 * Setting a successUrl would redirect the user there. Instead,
+			 * let's send 200 and the userDto.
+			 *****************************************/
+			.successHandler(authenticationSuccessHandler)
+			
+			/*******************************************
+			 * Setting the failureUrl will redirect the user to
+			 * that url if login fails. Instead, we need to send
+			 * 401. So, let's set failureHandler instead.
+			 *******************************************/
+        	.failureHandler(authenticationFailureHandler());
+	}
+
+
+	/**
+	 * Logout related configuration
+	 * 
+	 * @param http
+	 * @throws Exception
+	 */
+	protected void logout(HttpSecurity http) throws Exception {
+		
+		http
+		.logout()
+			
+			/************************************************
+			 * To prevent redirection to home page, we need to
+			 * have this custom logoutSuccessHandler
+			 ***********************************************/
+			.logoutSuccessHandler(logoutSuccessHandler)
+			.invalidateHttpSession(true)
+			.deleteCookies("JSESSIONID");
+	}
+
+	
+	/**
+	 * Configures exception-handling
+	 * 
+	 * @param http
+	 * @throws Exception
+	 */
+	protected void exceptionHandling(HttpSecurity http) throws Exception {
+		
+		http
+		.exceptionHandling()
+		
+			/***********************************************
+			 * To prevent redirection to the login page
+			 * when someone tries to access a restricted page
+			 **********************************************/
+			.authenticationEntryPoint(new Http403ForbiddenEntryPoint());
+	}
+
+
+	/**
+	 * Configures remember-me
+	 * 
+	 * @param http
+	 * @throws Exception
+	 */
+	protected void rememberMe(HttpSecurity http) throws Exception {
+		
+		http
+			.rememberMe()
+				.key(properties.getRememberMeKey())
+				.rememberMeServices(rememberMeServices());
+	}
+
+	
+	/**
+	 * Configures CSRF
+	 *  
+	 * @param http
+	 * @throws Exception
+	 */
+	protected void csrf(HttpSecurity http) throws Exception {
+		
+		http
+			.csrf()
+				.csrfTokenRepository(csrfTokenRepository())
+				.and()
+			.addFilterAfter(new LemonCsrfFilter(), CsrfFilter.class);
+	}
+
+	
+	/**
+	 * Adds switch-user filter
+	 * 
+	 * @param http
+	 */
+	protected void switchUser(HttpSecurity http) {
+		
+		http
+			.addFilterAfter(switchUserFilter(), FilterSecurityInterceptor.class);
+	}
+
+
+	/**
+	 * URL based authorization configuration. Override this if needed.
+	 * 
+	 * @param http
+	 * @throws Exception
+	 */
+	protected void authorizeRequests(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.mvcMatchers("/login/impersonate*").hasRole(GOOD_ADMIN)
+			.mvcMatchers("/logout/impersonate*").authenticated()
+			.mvcMatchers("/**").permitAll();                  
+	}
+	
+	
+	/**
+	 * Override this to add more http configurations,
+	 * such as more authentication methods.
+	 * 
+	 * @param http
+	 * @throws Exception
+	 */
+	protected void otherConfigurations(HttpSecurity http)  throws Exception {
+
+	}
+
+	
+	/**
+	 * Override this method if you want to 
+	 * setup a different RememberMeServices
+	 * 
+	 * @return
+	 */
+	protected AbstractRememberMeServices rememberMeServices() {
+    	
+        TokenBasedRememberMeServices rememberMeServices =
+        	new TokenBasedRememberMeServices
+        		(properties.getRememberMeKey(), userDetailsService);
+        rememberMeServices.setParameter(REMEMBER_ME_PARAMETER); // default is "remember-me" (in earlier spring security versions it was "_spring_security_remember_me")
+        rememberMeServices.setCookieName(REMEMBER_ME_COOKIE);
+        return rememberMeServices;
+        
+    }
+    
+
+	/**
+	 * A customized CsrfTokenRepository, for making it
+	 * compatible to AngularJS CSRF token header name.
+	 * Override this if you want to change the 
+	 * header name.
+	 *  
+	 * @return
+	 */
+	protected CsrfTokenRepository csrfTokenRepository() {
+		
+		HttpSessionCsrfTokenRepository repository =
+				new HttpSessionCsrfTokenRepository();
+		repository.setHeaderName(LemonCsrfFilter.XSRF_TOKEN_HEADER_NAME);
+		return repository;
+	}
+	
+	
+	/**
+	 * Returns switch-user filter
+	 * 
+	 * @return
+	 */
+	protected SwitchUserFilter switchUserFilter() {
+		SwitchUserFilter filter = new SwitchUserFilter();
+		filter.setUserDetailsService(userDetailsService);
+		filter.setSuccessHandler(authenticationSuccessHandler);
+		filter.setFailureHandler(authenticationFailureHandler());
+		return filter;
+	}	
+}
